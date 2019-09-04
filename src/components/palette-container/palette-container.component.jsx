90d7ad09ec0198e7ff4b6cd65bdec0e4b1eb129a
kine1991 <kine4599@gmail.com>
2019-09-04 12:30:22 +0300

    redo theme pages into context 2

--- src/components/palette-container/palette-container.component.jsx
+++ src/components/palette-container/palette-container.component.jsx
@@ -5,20 +5,19 @@ import Slider from '@material-ui/core/Slider';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
-// import Button from '@material-ui/core/Button';
 import { useStyles } from './palette-container.styles'
+import { ThemeContext } from '../../contexts/theme.context'
 
-
-
-const PaletteContainer = ({ setColor, color, typeElement}) => {
-    const classes = useStyles({color})
+const PaletteContainer = ({ /*setColor, color,*/ typeElement}) => {
+    const {currentPalette, setCurrentPalette} = React.useContext(ThemeContext)
+    
+    const classes = useStyles({currentPalette})
     const [level, setLevel] = React.useState(400)
     const [valueSlider, setValueSlider] = React.useState(5)
     const [valueHexColor, setValueHexColor] = React.useState('')
     const [selectedHexColor, setSelectedHexColor] = React.useState('')
 
     const map = {1: 50, 2: 100, 3: 200, 4: 300, 5: 400, 6: 500, 7: 600, 8: 700, 9: 800, 10: 900, 11: "A100", 12: "A200", 13: "A400", 14: "A700"}
-
 
     const changeLevel = (e, value) => {
         setLevel(map[value])
@@ -33,7 +32,7 @@ const PaletteContainer = ({ setColor, color, typeElement}) => {
         setValueHexColor(selectedHexColor)
         // selectedHexColor - изначально не выбран null
         if(selectedHexColor){
-            setColor({...color, [typeElement]: selectedHexColor})    
+            setCurrentPalette({...currentPalette, [typeElement]: selectedHexColor})    
         }
     }, [selectedHexColor])
 
@@ -54,3 +53,59 @@ const PaletteContainer = ({ setColor, color, typeElement}) => {
 }
 
 export default PaletteContainer
+
+
+
+
+// import React from 'react'
+// import * as colors from '@material-ui/core/colors';
+// import PaletteList from '../palette-list/palette-list.component'
+// import Slider from '@material-ui/core/Slider';
+// import FormControl from '@material-ui/core/FormControl';
+// import Input from '@material-ui/core/Input';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import { useStyles } from './palette-container.styles'
+
+// const PaletteContainer = ({ setColor, color, typeElement}) => {
+//     const classes = useStyles({color})
+//     const [level, setLevel] = React.useState(400)
+//     const [valueSlider, setValueSlider] = React.useState(5)
+//     const [valueHexColor, setValueHexColor] = React.useState('')
+//     const [selectedHexColor, setSelectedHexColor] = React.useState('')
+
+//     const map = {1: 50, 2: 100, 3: 200, 4: 300, 5: 400, 6: 500, 7: 600, 8: 700, 9: 800, 10: 900, 11: "A100", 12: "A200", 13: "A400", 14: "A700"}
+
+//     const changeLevel = (e, value) => {
+//         setLevel(map[value])
+//         setValueSlider(value)
+//     }
+
+//     const handleChange = (e) => {
+//         setValueHexColor(e.target.value)
+//     }
+
+//     React.useEffect(() => {
+//         setValueHexColor(selectedHexColor)
+//         // selectedHexColor - изначально не выбран null
+//         if(selectedHexColor){
+//             setColor({...color, [typeElement]: selectedHexColor})    
+//         }
+//     }, [selectedHexColor])
+
+//     return (
+//         <div className={classes.paletteContainer}>
+//             <FormControl className={classes.formControl}>
+//                 <InputLabel htmlFor="component-simple">Hex</InputLabel>
+//                 <Input className={classes.input} id="component-simple" disabled /*margin="dense"*/ value={valueHexColor} onChange={handleChange} />
+//             </FormControl>
+//             <Slider className={classes.slider} onChange={changeLevel} value={valueSlider} aria-labelledby="discrete-slider"  step={1} marks min={1} max={14}/>
+//             <div className={classes.lvlAndTypeEl}>
+//                 <div>Level: {level}</div>
+//                 <div>{typeElement}</div>
+//             </div>
+//             <PaletteList selectedHexColor={selectedHexColor} setSelectedHexColor={setSelectedHexColor} level={level} colors={colors}/>
+//         </div>
+//     )
+// }
+
+// export default PaletteContainer
