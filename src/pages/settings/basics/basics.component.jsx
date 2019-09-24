4b66afebc6b1231661f479bd0dc0f42026621db8
kine1991 <kine4599@gmail.com>
2019-09-24 17:56:37 +0300

    correct fill form from database basics page

--- src/pages/settings/basics/basics.component.jsx
+++ src/pages/settings/basics/basics.component.jsx
@@ -4,7 +4,9 @@ import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 import DateFnsUtils from '@date-io/date-fns';
-import {MuiPickersUtilsProvider, KeyboardTimePicker,KeyboardDatePicker} from '@material-ui/pickers';
+import {MuiPickersUtilsProvider, /*KeyboardTimePicker,*/ KeyboardDatePicker} from '@material-ui/pickers';
+import { firestore } from '../../../firebase/firebase';
+import { AuthContext } from '../../../contexts/auth.context'
 
 const useStyles = makeStyles(theme => ({
     paper: {
@@ -30,6 +32,9 @@ const useStyles = makeStyles(theme => ({
 
 const Text = () => {
     const [values, setValues] = React.useState({displayName: '', photoURL: '', location: ''});
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2018-08-18'));
+
+    const { userData } = React.useContext(AuthContext);
 
     const handleChange = (e) => {
       const { name, value } = e.target;
@@ -38,14 +43,28 @@ const Text = () => {
 
     const handleSubmitUpdatePrifile = (e) => {
       e.preventDefault()
-      console.log(values.displayName, values.photoURL)
+      
     }
-
-    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
 
     const handleDateChange = date => {
       setSelectedDate(date);
     };
+
+    React.useEffect(() => {
+      async function fetchData() {
+        try {
+            const userDataRef = firestore.doc(`users/${userData.uid}`);
+            const x = await userDataRef.get()
+            const { displayName, photoURL, location, birthday } = x.data();
+            console.log(displayName, photoURL, location, birthday.toDate());
+            setSelectedDate(birthday.toDate())
+            
+        } catch (e) {
+            console.error(e);
+        }
+    };
+    fetchData();
+    }, []);
 
     const classes = useStyles()
     return (
@@ -55,13 +74,11 @@ const Text = () => {
               <ValidatorForm className={classes.form} onSubmit={handleSubmitUpdatePrifile}>
                   <TextValidator name="displayName" placeholder="Display Name" variant="outlined" margin="dense" fullWidth value={values.displayName} onChange={handleChange} />
                   <TextValidator name="photoURL" placeholder="Photo URL" variant="outlined" margin="dense" fullWidth value={values.photoURL} onChange={handleChange} />
-                  <TextValidator name="loaction" placeholder="Loaction" variant="outlined" margin="dense" fullWidth value={values.loaction} onChange={handleChange} />
-
+                  <TextValidator name="location" placeholder="Loaction" variant="outlined" margin="dense" fullWidth value={values.location} onChange={handleChange} />
 
                   <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                    <KeyboardDatePicker disableToolbar variant="inline" format="MM/dd/yyyy" margin="normal" id="date-picker-inline" label="Date picker inline" value={selectedDate} onChange={handleDateChange} KeyboardButtonProps={{'aria-label': 'change date',}}/>
+                    <KeyboardDatePicker disableToolbar variant="inline" format="dd/MM/yyyy" margin="normal" id="date-picker-inline" label="Date picker inline" value={selectedDate} onChange={handleDateChange} KeyboardButtonProps={{'aria-label': 'change date',}}/>
                     {/* <KeyboardDatePicker margin="normal" id="date-picker-dialog" label="Date picker dialog" format="MM/dd/yyyy" value={selectedDate} onChange={handleDateChange} KeyboardButtonProps={{ 'aria-label': 'change date',}}/> */}
-
                   </MuiPickersUtilsProvider>
 
                   <Button className={classes.button} fullWidth color="primary" variant="contained"  type="submit">Submit</Button>
